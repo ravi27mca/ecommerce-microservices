aa624801e4e096af14aec76abde0c43a98ccfb10
unknown <ravi27mca@gmail.com>
2026-06-25 12:13:09 +0530

    user service

---
+++ user-service/src/main/java/com/my/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.my.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
